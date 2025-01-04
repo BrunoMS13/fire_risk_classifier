f126386587996cf1acd1b6c0bb6caac4c30d448e
Bruno <bme.silva@campus.fct.unl.pt>
2025-01-04 23:51:56 +0000

    small commit

--- test.sh
+++ test.sh
@@ -5,3 +5,5 @@ docker build -t fire_risk_classifier_image .
 
 # Run the container, mounting the cnn_checkpoint_weights folder
 docker run -it --rm -v "tmp/fire_risk_classifier/data/cnn_checkpoint_weights:/app/fire_risk_classifier/data/cnn_checkpoint_weights" fire_risk_classifier_image poetry run train --algorithm densenet --batch_size 32 --train True
+
+cp -r tmp/fire_risk_classifier/data/cnn_checkpoint_weights/* ~/models
