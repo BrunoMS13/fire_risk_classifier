d6ab34e1680725a422dfda7584142850a8b9e0cc
Bruno <bme.silva@campus.fct.unl.pt>
2025-01-05 12:07:42 +0000

    commit f

--- new.sh
+++ new.sh
@@ -1,8 +1,6 @@
 echo "Building Image..."
 
 # Build the Docker image
-docker build -t fire_risk_classifier_image .
-
 docker run -it --rm -v "/tmp/fire_risk_classifier/fire_risk_classifier/data/cnn_checkpoint_weights:/app/fire_risk_classifier/data/cnn_checkpoint_weights" fire_risk_classifier_image poetry run train --algorithm densenet --num_epochs 5 --batch_size 32 --train True --class_weights class_weights --fine_tunning True --load_weights /app/fire_risk_classifier/data/cnn_checkpoint_weights/densenet_body_2C.pth
 
 docker run -it --rm -v "/tmp/fire_risk_classifier/fire_risk_classifier/data/cnn_checkpoint_weights:/app/fire_risk_classifier/data/cnn_checkpoint_weights" fire_risk_classifier_image poetry run train --algorithm densenet --num_epochs 5 --batch_size 32 --train True --fine_tunning True --load_weights /app/fire_risk_classifier/data/cnn_checkpoint_weights/densenet_body_2C.pth
