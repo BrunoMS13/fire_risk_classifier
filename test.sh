54a8bb6ce6e5be02825f1406303fdcb3abdb2f1b
Bruno <bme.silva@campus.fct.unl.pt>
2025-01-05 00:14:58 +0000

    qf

--- test.sh
+++ test.sh
@@ -4,10 +4,10 @@ echo "Building Image..."
 docker build -t fire_risk_classifier_image .
 
 # Run the container, mounting the cnn_checkpoint_weights folder
-docker run -it --rm -v "tmp/fire_risk_classifier:/app/fire_risk_classifier/data/cnn_checkpoint_weights" fire_risk_classifier_image poetry run train --algorithm densenet --batch_size 32 --train True
+docker run -it --rm -v "/tmp/fire_risk_classifier:/app/fire_risk_classifier/data/cnn_checkpoint_weights" fire_risk_classifier_image poetry run train --algorithm densenet --batch_size 32 --train True
 
 cp -r /tmp/fire_risk_classifier/resnet_body_2C.pth ~/rgb_models_new/
 
-docker run -it --rm -v "tmp/fire_risk_classifier:/app/fire_risk_classifier/data/cnn_checkpoint_weights" fire_risk_classifier_image poetry run train --algorithm resnet --batch_size 64 --train True
+docker run -it --rm -v "/tmp/fire_risk_classifier:/app/fire_risk_classifier/data/cnn_checkpoint_weights" fire_risk_classifier_image poetry run train --algorithm resnet --batch_size 64 --train True
 
 cp -r /tmp/fire_risk_classifier/densenet_body_2C.pth ~/rgb_models_new/
