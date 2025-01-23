fe4c7cf5de2a12c84e9171b5b553d869fbfc14ad
Bruno <bme.silva@campus.fct.unl.pt>
2025-01-23 15:04:54 +0000

    quick update

--- benchmark_resnet.sh
+++ benchmark_resnet.sh
@@ -9,10 +9,24 @@ echo "Benchmark training resnets..."
 
 docker run -it --rm -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm resnet --batch_size 64 --train True
 
+echo "Copying resnet50..."
+
+mkdir -p ~/models/resnet
+cp -r $WEIGHTS_PATH/* ~/models/resnet
+
 docker run -it --rm -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm resnet101 --batch_size 64 --train True
+
+echo "Copying resnet101..."
+
+mkdir -p ~/models/resnet101
+cp -r $WEIGHTS_PATH/* ~/models/resnet101
 
 docker run -it --rm -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm resnet152 --batch_size 64 --train True
 
-echo "Copying resnet models and metrics ~/models/..."
+echo "Copying resnet152..."
 
-cp -r $WEIGHTS_PATH/* ~/models
+mkdir -p ~/models/resnet152
+cp -r $WEIGHTS_PATH/* ~/models/resnet152
+
+echo "Benchmark training resnets... Done"
+
