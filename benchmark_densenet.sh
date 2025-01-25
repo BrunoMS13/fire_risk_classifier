e3641e0c3945b9c551e3a0053b2adfdafe10db33
Bruno <bme.silva@campus.fct.unl.pt>
2025-01-25 12:22:13 +0000

    fix benchmark script

--- benchmark_densenet.sh
+++ benchmark_densenet.sh
@@ -12,19 +12,23 @@ docker run -it --rm -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier
 echo "Copying densenet161..."
 
 mkdir -p ~/models/densenet161
-cp -r $WEIGHTS_PATH/* ~/models/densenet161
+cp -r $WEIGHTS_PATH/densenet_body_2C.pth ~/models/densenet161
+cp -r $WEIGHTS_PATH/densenet_body_2C_metrics.json ~/models/densenet161
 
 docker run -it --rm -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm densenet169 --batch_size 32 --train True --num_epochs 12
 
 echo "Copying densenet169..."
 
 mkdir -p ~/models/densenet169
-cp -r $WEIGHTS_PATH/* ~/models/densenet169
+cp -r $WEIGHTS_PATH/densenet169_body_2C.pth ~/models/densenet169
+cp -r $WEIGHTS_PATH/densenet169_body_2C_metrics.json ~/models/densenet169
+
 
 docker run -it --rm -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm densenet201 --batch_size 32 --train True --num_epochs 12
 
 echo "Copying densenet201..."
 
-mkdir -p ~/models/resnet
-cp -r $WEIGHTS_PATH/* ~/models/densenet201
+mkdir -p ~/models/densenet201
+cp -r $WEIGHTS_PATH/densenet201_body_2C.pth ~/models/densenet201
+cp -r $WEIGHTS_PATH/densenet201_body_2C_metrics.json ~/models/densenet201
 
