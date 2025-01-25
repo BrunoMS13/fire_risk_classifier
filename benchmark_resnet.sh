582e88c0b36934b6590cf8341af6336120fa8701
Bruno <bme.silva@campus.fct.unl.pt>
2025-01-25 11:47:35 +0000

    commit atemp to fully passive run

--- benchmark_resnet.sh
+++ benchmark_resnet.sh
@@ -12,22 +12,24 @@ docker run -it --rm -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier
 echo "Copying resnet50..."
 
 mkdir -p ~/models/resnet50
-cp -r $WEIGHTS_PATH/* ~/models/resnet50
+cp -r $WEIGHTS_PATH/resnet_body_2C.pth ~/models/resnet50
+cp -r $WEIGHTS_PATH/resnet_body_2C_metrics.json ~/models/resnet50
 
 docker run -it --rm -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm resnet101 --batch_size 64 --train True --num_epochs 12
 
 echo "Copying resnet101..."
 
 mkdir -p ~/models/resnet101
-cp -r $WEIGHTS_PATH/resnet_body_2C.pth ~/models/resnet101
-cp -r $WEIGHTS_PATH/resnet_body_2C_metrics.json ~/models/resnet101
+cp -r $WEIGHTS_PATH/resnet101_body_2C.pth ~/models/resnet101
+cp -r $WEIGHTS_PATH/resnet101_body_2C_metrics.json ~/models/resnet101
 
 docker run -it --rm -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm resnet152 --batch_size 64 --train True --num_epochs 12
 
 echo "Copying resnet152..."
 
 mkdir -p ~/models/resnet152
-cp -r $WEIGHTS_PATH/* ~/models/resnet152
+cp -r $WEIGHTS_PATH/resnet152_body_2C.pth ~/models/resnet152
+cp -r $WEIGHTS_PATH/resnet152_body_2C_metrics.json ~/models/resnet152
 
 echo "Benchmark training resnets... Done"
 
