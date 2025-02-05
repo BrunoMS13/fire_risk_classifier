c4b9d6045d8a6ef6cdb9a3bf80f23775c251beb0
Bruno <bme.silva@campus.fct.unl.pt>
2025-02-05 14:20:21 +0000

    commit

--- fire_risk_classifier/scripts/full_test_hydra.py
+++ fire_risk_classifier/scripts/full_test_hydra.py
@@ -4,8 +4,9 @@ from fire_risk_classifier.utils.logger import Logger
 from fire_risk_classifier.dataclasses.params import Params
 
 model_names = [
+    "densenet161_body_2C.pth",
     "efficientnet_b4_body_2C.pth",
-    "efficientnet_b5_body_2C.pth"
+    "efficientnet_b5_body_2C.pth",
 ]
 
 NUM_CLASSES = 2
