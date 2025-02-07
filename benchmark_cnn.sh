1909177795b480cb44ef65f7a95eb5e809215694
Bruno <bme.silva@campus.fct.unl.pt>
2025-02-07 21:17:36 +0000

    commit

--- benchmark_cnn.sh
+++ benchmark_cnn.sh
@@ -9,7 +9,7 @@ mkdir -p ~/models/2C
 
 docker build -t fire_risk_classifier_image .
 
-docker run -it --rm --gpus all -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm efficientnet_b5 --batch_size 16 --train True --num_epochs 12 --num_classes $NUM_CLASSES --images_dir $IMAGE_DIR --wd 1e-4 --ndvi True --lr 5e-5
+docker run -it --rm --gpus all -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm efficientnet_b5 --batch_size 16 --train True --num_epochs 12 --num_classes $NUM_CLASSES --images_dir $IMAGE_DIR --wd 1e-4 --ndvi True --lr 1e-4
 
 echo "Copying efficientnet_b5..."
 
@@ -17,7 +17,7 @@ mkdir -p ~/models/2C/efficientnet_b5_ndvi_3e4
 cp -r $WEIGHTS_PATH/efficientnet_b5_body_2C.pth ~/models/2C/efficientnet_b5_ndvi_3e4
 cp -r $WEIGHTS_PATH/efficientnet_b5_body_2C_metrics.json ~/models/2C/efficientnet_b5_ndvi_3e4
 
-docker run -it --rm --gpus all -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm densenet161 --batch_size 16 --train True --num_epochs 12 --num_classes $NUM_CLASSES --images_dir $IMAGE_DIR --wd 1e-4 --ndvi True --lr 5e-5
+docker run -it --rm --gpus all -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm densenet161 --batch_size 16 --train True --num_epochs 12 --num_classes $NUM_CLASSES --images_dir $IMAGE_DIR --wd 1e-4 --ndvi True --lr 1e-4
 
 echo "Copying densenet161..."
 
