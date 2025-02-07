949835ddcbc66fcefa27dfc23a0889c3a6cdaf01
Bruno <bme.silva@campus.fct.unl.pt>
2025-02-07 20:07:30 +0000

    qf

--- init.sh
+++ init.sh
@@ -4,25 +4,21 @@
 set -e
 
 # Set dataset type (Change to "IRG" if needed)
-DATASET_TYPE="RGB"  # Change this to "IRG" if needed
+DATASET_TYPE="IRG"  # Change this to "IRG" if needed
 
 # Define dataset folder based on type
 DATASET_FOLDER="ortos2018-${DATASET_TYPE}-62_5m-decompressed"
 
-# Clone the GitHub repository
-echo "Cloning the fire_risk_classifier repository..."
-git clone https://github.com/BrunoMS13/fire_risk_classifier.git
-
 # Navigate to the appropriate directory
-cd fire_risk_classifier/fire_risk_classifier/data || { echo "Error: Directory not found!"; exit 1; }
+cd ~/fire_risk_classifier/fire_risk_classifier/data || { echo "Error: Directory not found!"; exit 1; }
 
 # Create the images directory if it doesn't exist
 mkdir -p images
 
-# Check if the source directory exists and copy it
+# Check if the source directory exists and copy it into images/
 if [ -d ~/fire_risk_classifier/fire_risk_classifier/data/images/$DATASET_FOLDER ]; then
-    echo "Copying $DATASET_FOLDER into data..."
-    cp -r ~/fire_risk_classifier/fire_risk_classifier/data/images/$DATASET_FOLDER .
+    echo "Copying $DATASET_FOLDER into images/ directory..."
+    cp -r ~/fire_risk_classifier/fire_risk_classifier/data/images/$DATASET_FOLDER images/
     echo "Files copied successfully!"
 else
     echo "Error: Source folder '$DATASET_FOLDER' does not exist!"
