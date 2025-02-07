4fa11de2e2adcf9981ec7232232903d7283ab12e
Bruno <bme.silva@campus.fct.unl.pt>
2025-02-07 20:30:53 +0000

    qf

--- benchmark_cnn.sh
+++ benchmark_cnn.sh
@@ -9,14 +9,6 @@ mkdir -p ~/models/2C
 
 docker build -t fire_risk_classifier_image .
 
-docker run -it --rm --gpus all -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm efficientnet_b5 --batch_size 16 --train True --num_epochs 12 --num_classes $NUM_CLASSES --images_dir $IMAGE_DIR --wd 3e-4 --ndvi True
-
-echo "Copying efficientnet_b5..."
-
-mkdir -p ~/models/2C/efficientnet_b5_ndvi_3e4
-cp -r $WEIGHTS_PATH/densenet161_body_2C.pth ~/models/2C/efficientnet_b5_ndvi_3e4
-cp -r $WEIGHTS_PATH/densenet161_body_2C_metrics.json ~/models/2C/efficientnet_b5_ndvi_3e4
-
 docker run -it --rm --gpus all -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm densenet161 --batch_size 16 --train True --num_epochs 12 --num_classes $NUM_CLASSES --images_dir $IMAGE_DIR --wd 3e-4 --ndvi True
 
 echo "Copying densenet161..."
