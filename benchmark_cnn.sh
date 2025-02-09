72a66dd1165b5d50d772d37bfd23d39d51517763
Bruno <bme.silva@campus.fct.unl.pt>
2025-02-09 11:57:05 +0000

    commitze benchmark

--- benchmark_cnn.sh
+++ benchmark_cnn.sh
@@ -5,25 +5,35 @@ DOCKER_WEIGHTS_PATH="/app/fire_risk_classifier/data/cnn_checkpoint_weights"
 IMAGE_DIR="fire_risk_classifier/data/images/ortos2018-IRG-62_5m-decompressed"
 
 NUM_CLASSES=2
-mkdir -p ~/models/2C
+mkdir -p ~/models
 
 docker build -t fire_risk_classifier_image .
 
-docker run -it --rm --gpus all -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm efficientnet_b5 --batch_size 16 --train True --num_epochs 12 --num_classes $NUM_CLASSES --images_dir $IMAGE_DIR --wd 1e-5 --ndvi True --lr 1e-4
+docker run -it --rm --gpus all -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm efficientnet_b5 --batch_size 16 --train True --num_epochs 12 --num_classes $NUM_CLASSES --images_dir $IMAGE_DIR --wd 1e-5 --lr 1e-4
 
 echo "Copying efficientnet_b5..."
 
-mkdir -p ~/models/2C/efficientnet_b5_ndvi_wd1e5_lr1e4
-cp -r $WEIGHTS_PATH/efficientnet_b5_body_2C.pth ~/models/2C/efficientnet_b5_ndvi_wd1e5_lr1e4
-cp -r $WEIGHTS_PATH/efficientnet_b5_body_2C_metrics.json ~/models/2C/efficientnet_b5_ndvi_wd1e5_lr1e4
+mkdir -p ~/models/efficientnet_b5_wd1e5_lr1e4
+cp -r $WEIGHTS_PATH/efficientnet_b5_body_2C.pth ~/models/efficientnet_b5_wd1e5_lr1e4
+cp -r $WEIGHTS_PATH/efficientnet_b5_body_2C_metrics.json ~/models/efficientnet_b5_wd1e5_lr1e4
 
-docker run -it --rm --gpus all -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm densenet161 --batch_size 16 --train True --num_epochs 12 --num_classes $NUM_CLASSES --images_dir $IMAGE_DIR --wd 1e-5 --ndvi True --lr 1e-4
+docker run -it --rm --gpus all -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm densenet161 --batch_size 16 --train True --num_epochs 12 --num_classes $NUM_CLASSES --images_dir $IMAGE_DIR --wd 1e-5 --lr 1e-4
 
 echo "Copying densenet161..."
 
-mkdir -p ~/models/2C/densenet161_ndvi_wd1e5_lr1e4
-cp -r $WEIGHTS_PATH/densenet161_body_2C.pth ~/models/2C/densenet161_ndvi_wd1e5_lr1e4
-cp -r $WEIGHTS_PATH/densenet161_body_2C_metrics.json ~/models/2C/densenet161_ndvi_wd1e5_lr1e4
+mkdir -p ~/models/densenet161_wd1e5_lr1e4
+cp -r $WEIGHTS_PATH/densenet161_body_2C.pth ~/models/densenet161_wd1e5_lr1e4
+cp -r $WEIGHTS_PATH/densenet161_body_2C_metrics.json ~/models/densenet161_wd1e5_lr1e4
+
+#resnet101
+
+docker run -it --rm --gpus all -v "$WEIGHTS_PATH:$DOCKER_WEIGHTS_PATH" fire_risk_classifier_image poetry run train --algorithm resnet101 --batch_size 16 --train True --num_epochs 12 --num_classes $NUM_CLASSES --images_dir $IMAGE_DIR --wd 1e-5 --lr 1e-4
+
+echo "Copying resnet101..."
+
+mkdir -p ~/models/resnet101_wd1e5_lr1e4
+cp -r $WEIGHTS_PATH/resnet101_body_2C.pth ~/models/resnet101_wd1e5_lr1e4
+cp -r $WEIGHTS_PATH/resnet101_body_2C_metrics.json ~/models/resnet101_wd1e5_lr1e4
 
 echo "Benchmark trainings complete!"
 
